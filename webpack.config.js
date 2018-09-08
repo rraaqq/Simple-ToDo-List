d48170721e0994ab9acb4356c9e3ddc60a295e2f
Michal Raczkowski <michal.raczkowski.88@gmail.com>
2018-09-08 18:32:27 +0100

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+const plugins = [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
 
 module.exports = (env) => {
     const environment = env || 'production';
+    if (env === 'production') {
+        plugins.push(
+            new OptimizeJsPlugin({
+                sourceMap: false
+            })
+        )
+    }
 
     return {
         mode: environment,
@@ -10,6 +25,7 @@ module.exports = (env) => {
             path: path.resolve(__dirname, 'build'),
             filename: 'app.' + environment + '.bundle.js'
         },
+    plugins,
     module: {
         rules: [
             {
