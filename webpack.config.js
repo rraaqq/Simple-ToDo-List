61068c634740356255752df7759963f4ec357127
Michal Raczkowski <michal.raczkowski.88@gmail.com>
2018-09-08 18:04:53 +0100

    Task finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
+module.exports = (env) => {
+    const environment = env || 'production';
+
+    return {
+        mode: environment,
+        entry: './src/containers/App.js',
         output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.' + environment + '.bundle.js'
+        },
     module: {
         rules: [
             {
@@ -26,4 +30,5 @@ module.exports = {
             }
         ]
     }
+}
 };
